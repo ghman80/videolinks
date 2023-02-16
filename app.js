a429bb3ba411b226142175aba6934de2e5725f11
ghman80 <tienviba@yahoo.com>
2023-02-16 09:54:00 +0700

    remover commnet in json  for vercel 1

--- app.js
+++ app.js
@@ -132,7 +132,7 @@ app.use(function(req, res){
 
 /* istanbul ignore next */
 if (!module.parent) {
-  app.listen(3000);
+  app.listen(3000 || process.env.PORT);
   console.log('Express started on port 3000');
 }
 
