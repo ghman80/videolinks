aaf81436eae5d996190f59cbaba563d5bd2be5eb
ghman80 <tienviba@yahoo.com>
2023-03-08 09:18:32 +0700

    add thumb and title

--- app.js
+++ app.js
@@ -8,10 +8,21 @@ const axios = require('axios');
 var express = require('express');
 
 var app = module.exports = express();
+const fs = require('fs');
 
 // create an error with .status. we
 // can then use the property in our
 // custom error handler (Connect respects this prop as well)
+
+class fbVideoPost {
+  constructor(status , msg , url, title , thumb) {
+    this.status = status;
+    this.msg = msg;
+    this.url = url;
+    this.title = title;
+    this.thumb = thumb;
+  }
+}
 
 function error(status, msg) {
   var err = new Error(msg);
@@ -77,7 +88,7 @@ app.get('/api/users1', function(req, res, next){
   res.send(users);
 });
 
-// example: http://localhost:3000/api/geturl/?api-key=foo&url-fb=https://m.facebook.com/watch?v=890759372162366
+// example: http://localhost:3000/api/geturl/?api-key=foo&url-fb=https://m.facebook.com/watch?v=167296512751262
 app.get('/api/geturl/', function(req, res, next) {
 
   let urlvideo;
@@ -87,15 +98,20 @@ app.get('/api/geturl/', function(req, res, next) {
   getdownLoadLinks(urlvideo)
     .then(
       (urls) => {
-        const vdlink = urls[0].replaceAll("&amp;","&");
-        res.send([{'url':vdlink}]);
+        
+        const vdlink = (urls[0].url).replaceAll("&amp;","&");
+        const vdstatus = urls[0].status; 
+        const vdmsg = urls[0].msg;
+        const vdthumb = urls[0].thumb.replaceAll("&amp;","&");
+        const vdtitle = urls[0].title.replace(/(.+?)-/m,'').replace('| Facebook"','');
+        res.send({'status':vdstatus, 'message':vdmsg,'url':vdlink,'title':vdtitle,'thumb':vdthumb});
       });
 });
 
 // example: http://localhost:3000/api/repos/?api-key=foo
 app.get('/api/repos', function(req, res, next){
   
-  axios.get('https://m.facebook.com/watch?v=890759372162366').then((res) => {
+  axios.get('https://m.facebook.com/watch?v=167296512751262').then((res) => {
     
   console.log(res.data)});
      
@@ -146,11 +162,27 @@ const getdownLoadLinks = async (viewLink) => {
       method: 'get',
     // maxBodyLength: Infinity,
       url: viewLink,
+      // headers: { 
+      //   'authority': 'm.facebook.com', 
+      //   'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7', 
+      //   'accept-language': 'en-US,en;q=0.9', 
+      //   'cookie': 'sb=3wvrY-tCb7fA-2CovBly26vv; datr=3wvrY_KVkixtffPsfcdDmFpw; locale=en_GB; c_user=100007151746863; xs=22%3ACA-V4EN9FtUO-g%3A2%3A1676349568%3A-1%3A15345; m_page_voice=100007151746863; m_pixel_ratio=0.75; presence=EDvF3EtimeF1676349963EuserFA21B07151746863A2EstateFDutF0CEchF_7bCC; fr=0oxQ1GwS45hEfrCnJ.AWXTIQXKy3tDvcgmxe7idRrfF30.Bj6wvf.h2.AAA.0.0.Bj6xJI.AWWiobgCINs; x-referer=eyJyIjoiL2hvbWUucGhwP3BhaXB2PTAmZWF2PUFmYklKWDBmbTZlaGc3S0xwSEx4bTR5Q0hqYWVVYnZvSFpPQUs0MHVRQ2dsOE9RVE1TYzFKTW5PaUIzZmNYb19mNzQmdGJ1YT0xIiwiaCI6Ii9ob21lLnBocD9wYWlwdj0wJmVhdj1BZmJJSlgwZm02ZWhnN0tMcEhMeG00eUNIamFlVWJ2b0haT0FLNDB1UUNnbDhPUVRNU2MxSk1uT2lCM2ZjWG9fZjc0JnRidWE9MSIsInMiOiJtIn0%3D; wd=1381x929; datr=ZPvpY8psgAah7JRKAoBeOr2o; sb=ZPvpY1VbL4ECsd2CZEtwpKMU', 
+      //   'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"', 
+      //   'sec-ch-ua-mobile': '?0', 
+      //   'sec-ch-ua-platform': '"Windows"', 
+      //   'sec-fetch-dest': 'document', 
+      //   'sec-fetch-mode': 'navigate', 
+      //   'sec-fetch-site': 'none', 
+      //   'sec-fetch-user': '?1', 
+      //   'upgrade-insecure-requests': '1', 
+      //   'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+      // }
       headers: { 
         'authority': 'm.facebook.com', 
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7', 
         'accept-language': 'en-US,en;q=0.9', 
-        'cookie': 'sb=3wvrY-tCb7fA-2CovBly26vv; datr=3wvrY_KVkixtffPsfcdDmFpw; locale=en_GB; c_user=100007151746863; xs=22%3ACA-V4EN9FtUO-g%3A2%3A1676349568%3A-1%3A15345; m_page_voice=100007151746863; m_pixel_ratio=0.75; presence=EDvF3EtimeF1676349963EuserFA21B07151746863A2EstateFDutF0CEchF_7bCC; fr=0oxQ1GwS45hEfrCnJ.AWXTIQXKy3tDvcgmxe7idRrfF30.Bj6wvf.h2.AAA.0.0.Bj6xJI.AWWiobgCINs; x-referer=eyJyIjoiL2hvbWUucGhwP3BhaXB2PTAmZWF2PUFmYklKWDBmbTZlaGc3S0xwSEx4bTR5Q0hqYWVVYnZvSFpPQUs0MHVRQ2dsOE9RVE1TYzFKTW5PaUIzZmNYb19mNzQmdGJ1YT0xIiwiaCI6Ii9ob21lLnBocD9wYWlwdj0wJmVhdj1BZmJJSlgwZm02ZWhnN0tMcEhMeG00eUNIamFlVWJ2b0haT0FLNDB1UUNnbDhPUVRNU2MxSk1uT2lCM2ZjWG9fZjc0JnRidWE9MSIsInMiOiJtIn0%3D; wd=1381x929; datr=ZPvpY8psgAah7JRKAoBeOr2o; sb=ZPvpY1VbL4ECsd2CZEtwpKMU', 
+        'cache-control': 'max-age=0', 
+        'cookie': 'sb=DKoFZF8ptuGPr1f0y-vR6cmC; datr=DKoFZCDZKYaDZ6hAThUD1vMW; c_user=100007151746863; xs=36%3AC7_9ZEBcA14inQ%3A2%3A1678092819%3A-1%3A15345; fr=0bDZw5KiofiMJWnAr.AWW5_U29QGbex9RbPTLjt0Y7Bn0.BkBaoM.D5.AAA.0.0.BkBaoW.AWX6gm1fWO4; presence=C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1678092833768%2C%22v%22%3A1%7D; m_page_voice=100007151746863; m_pixel_ratio=0.75; wd=1693x929', 
         'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"', 
         'sec-ch-ua-mobile': '?0', 
         'sec-ch-ua-platform': '"Windows"', 
@@ -173,7 +205,8 @@ const getdownLoadLinks = async (viewLink) => {
 
       // Note: This regex is not a string ! 
       // const regex= /href="\/video_redirect\/\?src=(.+?)"/gm
-             const regex = /meta property="og:video" content="(.+?)"/gm
+      // const regex1 = /meta property="og:video" content="(.+?)"/gm
+      const regex = /<meta property="og:video:secure_url" content="(.+?)" \/><meta property="og:url" content="" \/><meta property="og:site_name" content="Facebook Watch" \/><meta property="og:title" content="(.+?) \/><meta property="og:image" content="(.+?)"/gm
       //const regex=/href="(.+?)"/gm
       //const regex= /<img src=("https:\/\/scontent.*?")/gm
       
@@ -189,9 +222,15 @@ const getdownLoadLinks = async (viewLink) => {
           // The result can be accessed through the `m`-variable. m is an Array
           m.forEach((match, groupIndex,arr) => {
               //console.log(`Found match, group ${groupIndex}: ${match}`);
-              urls.push(decodeURIComponent(arr[1]));
+              //let vdurl = decodeURIComponent(arr[1] + '- thumb:  ' + arr[3]);
+              let fb = new fbVideoPost(1,'Success',arr[1],arr[2],arr[3]);
+
+              urls.push(fb);
           });
+          
       }
+      //console.log(str); // console not write full content of str variable ! 
+      fs.writeFileSync('tst.txt', str); 
       return urls;
   } catch (error) {
       throw error;
